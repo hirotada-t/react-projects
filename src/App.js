ceea06563f4297fad76cacffc91914ef4cfa7448
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2022-10-04 18:27:54 +0900

    hooks promise

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import { useEffect, useState } from 'react';
+import { getAllPokemon,getPokemon } from './utils/pokemon.js';
 import './App.css';
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // すべてのポケモンデータを取得
+      // 各ポケモンの詳細データを取得
+      let res = await getAllPokemon(initialURL);
+      loadPokemon(res.results);
+      setLoading(false);
+    };
+    fetchPokemonData();
+  }, []);
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      {loading ? (
+        <h1>ロード中・・・</h1>
+      ) : (
+        <h1>ポケモンデータを取得しました。</h1>
+      )}
+    </div>
   );
 }
 
---
+++ study-log.md
@@ -0,0 +1,32 @@
+# 1004
+## React hooks
+- useEffect・・・画面が読み込まれた時に実行される処理内容
+## 非同期処理
+- fetch→then→then の処理が終了するまで待つ（非同期処理）
+- resolve→成功したときの処理
+- reject→失敗したときの処理
+- Promiseを使う場合はasync-awaitが必要
+- APIで外部からデータを取得する場合（＝通信を行う場合）
+const hogehoge = async () => {
+  await fugafuga = () => {
+    new Promise((resoleve, reject) => {
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => resoleve(data));
+    });
+  };
+};
+
+## useStateとローディング
+- useState・・・コンポーネントの状態を保持or変更
+例1）const [loading, setLoading] = useState(true);
+例2）const [値, 関数] = useState(初期値)
+- ロードの状態を保持→必要に応じてture/falseで状態を変更する
+- ローディングしているかどうか
+- 関数で実行して状態を更新する
+- 更新した内容を値に保持する
+- useStateの引数に初期値（＝最初の状態）を設定する
+
+## Promise.all(配列)
+- 配列内にあるすべてのfetchが完了するまで待つ
+- async-awaitが必要
