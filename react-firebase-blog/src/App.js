8e70f5c83491456ec6a9eba11b62d74bf81a649e
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2022-10-11 16:56:39 +0900

    log in

--- react-firebase-blog/src/App.js
+++ react-firebase-blog/src/App.js
@@ -5,15 +5,18 @@ import CreatePost from './Components/CreatePost';
 import Login from './Components/Login';
 import Logout from './Components/Logout';
 import Navbar from './Components/Navbar';
+import { useState } from 'react';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+
   return (
   <Router>
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/createpost" element={<CreatePost />}></Route>
-      <Route path="/login" element={<Login />}></Route>
+      <Route path="/login" element={<Login setIsAuth={setIsAuth} />}></Route>
       <Route path="/logout" element={<Logout />}></Route>
     </Routes>
   </Router>);
