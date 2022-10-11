517e6b5173857d4c5a56b3a3ef050f1b1515b792
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2022-10-11 17:18:24 +0900

    clear warning

--- react-firebase-blog/src/App.js
+++ react-firebase-blog/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from './Components/Home';
 import CreatePost from './Components/CreatePost';
 import Login from './Components/Login';
--- react-firebase-blog/src/Components/Navbar.js
+++ react-firebase-blog/src/Components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import "./Navbar.css"
-import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse, faFilePen, faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons'
 
